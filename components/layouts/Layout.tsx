cc632033bdcc960c011b4dd9dd68f9d0cb930e91
FedeL <FedeL>
2022-05-27 20:41:32 -0300

    layout and navbar creation

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from 'react'
+import Head from 'next/head'
+
+import { Box } from '@mui/material'
+import { Navbar } from '../ui'
+
+interface Props {
+  title?: string
+  children: ReactNode
+}
+
+export const Layout: FC<Props> = ({ title = 'OpenNotion', children }) => {
+  return (
+    <Box sx={{ flexflow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navbar />
+      {/**SideBar */}
+      <Box sx={{ padding: '10px 20px' }}>{children}</Box>
+    </Box>
+  )
+}
