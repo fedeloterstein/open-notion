616551d6b616647c1c8087edfa1eabc894562cb1
FedeL <FedeL>
2022-05-30 01:36:13 -0300

    updateEntry api creation

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -23,8 +23,17 @@ export const EntriesProvider: FC<EntriesProviderPros> = ({ children }) => {
     dispatch({ type: '[Entry] Add-Entry', payload: data })
   }
 
-  const upDateEntry = (entry: Entry) => {
-    dispatch({ type: '[Entry] Entry-Updated', payload: entry })
+  const upDateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      })
+
+      dispatch({ type: '[Entry] Entry-Updated', payload: data })
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const refreshEntries = async () => {
